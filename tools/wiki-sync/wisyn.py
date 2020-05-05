949aee1a8d1745b922322422e08cc1a654f39a31
FrankyFire <sebastian.arndt95@gmail.com>
2020-05-05 15:30:51 +0000

    Translation is working now, added some descirption, changed behaviour of the tmp folder - will be named after customer prefix right now. Maybe not the final solution.

--- tools/wiki-sync/wisyn.py
+++ tools/wiki-sync/wisyn.py
@@ -34,20 +34,20 @@ import commands     #Zum ausführen von Kommdos auf der Shell, hier zur Prüfung
 customers = []
 
 # Standard-Werte:
-separator = '-'
+separator = '_'
 confDir = os.path.join(os.getcwd(), 'config.json')
-wikiDir = '~/var/dokuwiki/data/pages/'
-tmp = os.path.join(os.getcwd(), 'tmp/')
+wikiDir = os.path.expanduser('~/var/dokuwiki/data/pages/')	#Expanduser ersetzt ~ mit der Home-Directory
 
 # Eine Klasse Kunde, in der alle Kunden, welche in der Konfiguration angegeben wurden, hinterlegt werden.
 class Customer:
     def __init__(self, name, prefix, dir, local_subdir, host, user):
-        self.name = name                                # Kundenname
-        self.prefix = prefix                            # Prefix in lokalem CheckMK
-        self.dir = dir                                  # Wiki-Ordner im Kundensystem
-        self.local_subdir = local_subdir                # Unterordner in lokalem Wiki-Ordner
-        self.host = host                                # IP/Hostname des Kundensystems
-        self.credentials = user + '@' + self.host       # Anmeldedaten für Remote-Host (benutzer@remote-host)
+        self.name = name                                      # Kundenname
+        self.prefix = prefix                                  # Prefix in lokalem CheckMK
+        self.dir = dir                                        # Wiki-Ordner im Kundensystem
+        self.local_subdir = local_subdir                      # Unterordner in lokalem Wiki-Ordner
+        self.host = host                                      # IP/Hostname des Kundensystems
+        self.credentials = user + '@' + self.host             # Anmeldedaten für Remote-Host (benutzer@remote-host)
+        self.tmp = os.path.join(os.getcwd(), (prefix + '/'))  # TMP-Ordner und Kundenordner werden nach dem Präfix des Kunden benannt.
     
     def translate(self, to_temp):
         # Achtung: Unfertig!
@@ -64,25 +64,29 @@ class Customer:
         ##################################################
         
         if to_temp: # Wenn in lokale wikiDir geschrieben wird (eingehnde Synchronisation)...
-            for path, subdirs, files in os.walk(os.path.normpath(tmp)):     # Alle Dateien in Pfad, inklusive Unterordner, durchsuchen
+            for path, subdirs, files in os.walk(os.path.normpath(self.tmp)):     # Alle Dateien in Pfad, inklusive Unterordner, durchsuchen
                 for curpath in subdirs:                                     # Unterordner umbenennen (Präfix anfügen)
                     newsubpath = self.prefix + separator + curpath
                     os.rename(os.path.join(path, curpath), os.path.join(path, newsubpath))
                 for name in files:                                          # Dateien umbenennen (Präfix anfügen)
                     name = os.path.basename(name)
                     os.rename(os.path.join(path, name), os.path.join(path, (self.prefix + separator + name))) # Präfix anfügen
-                os.system('cp -R ' + path + ' ' + os.path.basename(wikiDir)) # Kopieren der Wiki-Einträge aus tmp in den realen Wiki-Ordner
+            os.system('cp -R ' + self.tmp + ' ' + wikiDir) # Kopieren der Wiki-Einträge aus tmp in den realen Wiki-Ordner
             return 1
         else: # Wenn in Temp geschrieben wird...
-            for path, subdirs, files in os.walk(os.path.normpath(wikiDir)):     # Alle Dateien in Pfad, inklusive Unterordnern, durchsuchen
-                search_string = self.prefix + separator
+            os.system('mkdir ' + self.tmp)
+            search_string = self.prefix + separator	      # Der Kundenpräfix, welcher gesucht und vor der synchronisation entfernt werden soll
+            for path, subdirs, files in os.walk(wikiDir):     # Alle Dateien in Pfad, inklusive Unterordnern, durchsuchen
                 prefix_size = len(search_string)
                 for curpath in subdirs:
                     if curpath[:prefix_size] == search_string:                                         # Wenn der Dateiname das Präfix des Kunden enthält...
-                        os.system('cp ' + os.path.join(path, curpath) + ' ' + tmp)                  # Kopieren der Wiki-Einträge nach tmp
-                        print(curpath)
-                        os.rename(os.path.join(tmp, curpath), os.path.join(tmp, curpath[search_size:])) # Präfix entfernen
-            print('Translation went wrong.')
+                        if os.path.isdir(os.path.join(path, curpath)):		# Wenn aktuelles Element ein Pfad ist...
+                            # Kopiere den gesamten Pfad und benenne dabei um...
+                            os.system('cp -R ' + os.path.join(path, curpath) + ' ' + self.tmp + curpath[prefix_size:])                  # Kopieren der Wiki-Einträge nach tmp
+                            # ...und durchsuche diesen Ordner noch nach dem Präfix
+                        else:
+                            # ...sonst nur die jeweilige Datei kopieren und dabei umbenennen.
+                            os.system('cp ' + os.path.join(path, curpath) + ' ' + self.tmp + curpath[prefix_size:])
             return 1
 
         return 0
@@ -105,15 +109,16 @@ class Customer:
             return 1
         
         # Bidirektionale synchronisation, erst vom Kunden zum ServiceDesk, danach umgekehrt.
-        os.system('rsync --update --numeric-ids -ravz' + ' ' + self.credentials + ':' + self.dir + ' ' + tmp)
+        os.system('rsync --update --numeric-ids -ravz' + ' ' + self.credentials + ':' + self.dir + ' ' + self.tmp)
 
         # Rücksynchronisation 
         if len(cust.prefix) > 0:
                 cust.translate(1)
+                os.system('rm -R ' + self.tmp)
                 cust.translate(0)
-        os.system('rsync --update --numeric-ids -ravz' + ' ' + tmp + ' ' + self.credentials + ':' + self.dir)
+        os.system('rsync --update --numeric-ids -ravz' + ' ' + self.tmp + ' ' + self.credentials + ':' + self.dir)
 
-        os.system('rm -R ' + tmp)       # Temp Ordner wieder entfernen
+        os.system('rm -R ' + self.tmp)       # Temp Ordner wieder entfernen
 
         return 0
 
@@ -137,7 +142,7 @@ def readConfig():
 
             # Lokale Konfiguration
             separator = content["lokal"]["prefix-separator"]
-            wikiDir = content["lokal"]["wiki-pfad"]
+            wikiDir = os.path.expanduser(content["lokal"]["wiki-pfad"])		#Expanduser ersetzt ~ mit der Home-Directory
 
             # Kundenkonfiguration: Für jeden Kunden...
             for company in content["kunden"]:
